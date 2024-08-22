6523bb3727a3769267bf4187c62119b9b68661cf
Fausto Emmanuel <fequevedot@gmail.com>
2024-08-22 17:21:04 -0600

    DTO y AutoMapper

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,3 +1,4 @@
+using MagicVilla_API;
 using MagicVilla_API.Datos;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDBcontext>(option => {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 var app = builder.Build();
 
